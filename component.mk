78c71ed24cb9426ac0087007ba0c3b5f10d37e09
Anderson <80588197+huishizhao@users.noreply.github.com>
2024-04-26 21:12:35 +0800

    Update component.mk

--- component.mk
+++ component.mk
@@ -3,7 +3,7 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-ifdef CONFIG_ESP32_KORVO_V1_BOARD
+ifdef CONFIG_ESP32_KORVO_V1_1_BOARD
 COMPONENT_ADD_INCLUDEDIRS += ./esp32_korvo_v1
 COMPONENT_SRCDIRS += ./esp32_korvo_v
 endif
